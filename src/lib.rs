35832e33854c81d0222e5b49c5601d9ed99a9947
Farrel Lifson <farrel.lifson@aimred.com>
2014-10-15 10:35:07 +0200

    Return a Result

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-pub fn split_into(dividend: uint, divisor: uint) -> Vec<uint> {
-    if divisor == 0 { return Vec::from_elem(0, dividend)};
+static DIVISOR_IS_ZERO: &'static str = "Divisor is zero";
+
+pub fn split_into(dividend: uint, divisor: uint) -> Result<Vec<uint>, &'static str> {
+    if divisor == 0 { return Err(DIVISOR_IS_ZERO)};
 
     let quotient = dividend / divisor;
     let remainder = dividend % divisor;
 
-    let mut parts: Vec<uint> = Vec::from_elem(divisor, quotient);
+    let mut parts: Vec<uint> = Vec::from_elem(divisor as uint, quotient);
 
     for x in range(0, remainder) {
         *parts.get_mut(x) = parts[x] + 1;
     }
 
     parts.reverse();
-    return parts;
+    return Ok(parts);
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -3,16 +3,20 @@ extern crate split_into;
 use split_into::split_into;
 
 #[test]
+fn test_split_into_zero() {
+    assert_eq!(Err("Divisor is zero"),        split_into(10,0));
+}
+
+#[test]
 fn test_split_into() {
-    assert_eq!(Vec::from_elem(0,10),      split_into(10,0));
-    assert_eq!(vec!(10),                  split_into(10,1));
-    assert_eq!(vec!(5,5),                 split_into(10,2));
-    assert_eq!(vec!(3,3,4),               split_into(10,3));
-    assert_eq!(vec!(2,2,3,3),             split_into(10,4));
-    assert_eq!(vec!(2,2,2,2,2),           split_into(10,5));
-    assert_eq!(vec!(1,1,2,2,2,2),         split_into(10,6));
-    assert_eq!(vec!(1,1,1,1,2,2,2),       split_into(10,7));
-    assert_eq!(vec!(1,1,1,1,1,1,2,2),     split_into(10,8));
-    assert_eq!(vec!(1,1,1,1,1,1,1,1,2),   split_into(10,9));
-    assert_eq!(vec!(1,1,1,1,1,1,1,1,1,1), split_into(10,10));
+    assert_eq!(Ok(vec!(10)),                  split_into(10,1));
+    assert_eq!(Ok(vec!(5,5)),                 split_into(10,2));
+    assert_eq!(Ok(vec!(3,3,4)),               split_into(10,3));
+    assert_eq!(Ok(vec!(2,2,3,3)),             split_into(10,4));
+    assert_eq!(Ok(vec!(2,2,2,2,2)),           split_into(10,5));
+    assert_eq!(Ok(vec!(1,1,2,2,2,2)),         split_into(10,6));
+    assert_eq!(Ok(vec!(1,1,1,1,2,2,2)),       split_into(10,7));
+    assert_eq!(Ok(vec!(1,1,1,1,1,1,2,2)),     split_into(10,8));
+    assert_eq!(Ok(vec!(1,1,1,1,1,1,1,1,2)),   split_into(10,9));
+    assert_eq!(Ok(vec!(1,1,1,1,1,1,1,1,1,1)), split_into(10,10));
 }
