f04a916f3dbd56f75ac3269e1ed41cee656d3ffa
Farrel Lifson <farrel.lifson@aimred.com>
2014-10-16 14:09:33 +0200

    Convert to a trait implementation on int

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,29 @@
 static DIVISOR_IS_ZERO: &'static str              = "Divisor is zero";
 static DIVISOR_LARGER_THAN_DIVIDEND: &'static str = "Divisor is larger than the dividend";
 
-pub fn split_into(dividend: int, divisor: uint) -> Result<Vec<int>, &'static str> {
-    if divisor == 0                     { return Err(DIVISOR_IS_ZERO) };
-    if dividend == 0                    { return Ok(Vec::from_elem(divisor,0i))};
-    if divisor > dividend.abs() as uint { return Err(DIVISOR_LARGER_THAN_DIVIDEND) }
+pub trait SplitInto {
+    fn split_into(&self, divisor: uint) -> Result<Vec<int>, &'static str>; 
+}
 
-    let quotient  = dividend / divisor as int;
-    let remainder = dividend.abs() as uint % divisor;
+impl SplitInto for int {
+    fn split_into(&self, divisor: uint) -> Result<Vec<int>, &'static str> {
+        let dividend = *self;
+        if divisor == 0                     { return Err(DIVISOR_IS_ZERO) };
+        if dividend == 0                    { return Ok(Vec::from_elem(divisor,0i)) };
+        if divisor > dividend.abs() as uint { return Err(DIVISOR_LARGER_THAN_DIVIDEND) }
 
-    let mut parts: Vec<int> = Vec::from_elem(divisor, quotient);
+        let quotient  = dividend / divisor as int;
+        let remainder = dividend.abs() as uint % divisor;
 
-    let adjustment = if dividend > 0 { 1i } else { -1i }; 
+        let mut parts: Vec<int> = Vec::from_elem(divisor, quotient);
 
-    for x in range(0, remainder as uint) {
-        *parts.get_mut(x) = parts[x] + adjustment;
+        let adjustment = if dividend > 0 { 1i } else if dividend < 0 { -1i } else { 0i }; 
+
+        for x in range(0, remainder as uint) {
+            *parts.get_mut(x) = parts[x] + adjustment;
+        }
+
+        parts.reverse();
+        return Ok(parts);
     }
-
-    parts.reverse();
-    return Ok(parts);
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,50 +1,50 @@
 extern crate split_into;
 
-use split_into::split_into;
+use split_into::SplitInto;
 
 #[test]
 fn test_split_into_zero_divisor() {
-    assert_eq!(Err("Divisor is zero"),        split_into(10,0));
+    assert_eq!(Err("Divisor is zero"),        0.split_into(0));
 }
 
 #[test]
 fn test_split_into_positive() {
-    assert_eq!(Ok(vec!(10)),                  split_into(10,1));
-    assert_eq!(Ok(vec!(5,5)),                 split_into(10,2));
-    assert_eq!(Ok(vec!(3,3,4)),               split_into(10,3));
-    assert_eq!(Ok(vec!(2,2,3,3)),             split_into(10,4));
-    assert_eq!(Ok(vec!(2,2,2,2,2)),           split_into(10,5));
-    assert_eq!(Ok(vec!(1,1,2,2,2,2)),         split_into(10,6));
-    assert_eq!(Ok(vec!(1,1,1,1,2,2,2)),       split_into(10,7));
-    assert_eq!(Ok(vec!(1,1,1,1,1,1,2,2)),     split_into(10,8));
-    assert_eq!(Ok(vec!(1,1,1,1,1,1,1,1,2)),   split_into(10,9));
-    assert_eq!(Ok(vec!(1,1,1,1,1,1,1,1,1,1)), split_into(10,10));
+    assert_eq!(Ok(vec!(10)),                  10.split_into(1));
+    assert_eq!(Ok(vec!(5,5)),                 10.split_into(2));
+    assert_eq!(Ok(vec!(3,3,4)),               10.split_into(3));
+    assert_eq!(Ok(vec!(2,2,3,3)),             10.split_into(4));
+    assert_eq!(Ok(vec!(2,2,2,2,2)),           10.split_into(5));
+    assert_eq!(Ok(vec!(1,1,2,2,2,2)),         10.split_into(6));
+    assert_eq!(Ok(vec!(1,1,1,1,2,2,2)),       10.split_into(7));
+    assert_eq!(Ok(vec!(1,1,1,1,1,1,2,2)),     10.split_into(8));
+    assert_eq!(Ok(vec!(1,1,1,1,1,1,1,1,2)),   10.split_into(9));
+    assert_eq!(Ok(vec!(1,1,1,1,1,1,1,1,1,1)), 10.split_into(10));
 }
 
 #[test]
 fn test_split_into_negative() {
-    assert_eq!(Ok(vec!(-10)),                           split_into(-10,1));
-    assert_eq!(Ok(vec!(-5,-5)),                         split_into(-10,2));
-    assert_eq!(Ok(vec!(-3,-3,-4)),                      split_into(-10,3));
-    assert_eq!(Ok(vec!(-2,-2,-3,-3)),                   split_into(-10,4));
-    assert_eq!(Ok(vec!(-2,-2,-2,-2,-2)),                split_into(-10,5));
-    assert_eq!(Ok(vec!(-1,-1,-2,-2,-2,-2)),             split_into(-10,6));
-    assert_eq!(Ok(vec!(-1,-1,-1,-1,-2,-2,-2)),          split_into(-10,7));
-    assert_eq!(Ok(vec!(-1,-1,-1,-1,-1,-1,-2,-2)),       split_into(-10,8));
-    assert_eq!(Ok(vec!(-1,-1,-1,-1,-1,-1,-1,-1,-2)),    split_into(-10,9));
-    assert_eq!(Ok(vec!(-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)), split_into(-10,10));
+    assert_eq!(Ok(vec!(-10)),                           (-10).split_into(1));
+    assert_eq!(Ok(vec!(-5,-5)),                         (-10).split_into(2));
+    assert_eq!(Ok(vec!(-3,-3,-4)),                      (-10).split_into(3));
+    assert_eq!(Ok(vec!(-2,-2,-3,-3)),                   (-10).split_into(4));
+    assert_eq!(Ok(vec!(-2,-2,-2,-2,-2)),                (-10).split_into(5));
+    assert_eq!(Ok(vec!(-1,-1,-2,-2,-2,-2)),             (-10).split_into(6));
+    assert_eq!(Ok(vec!(-1,-1,-1,-1,-2,-2,-2)),          (-10).split_into(7));
+    assert_eq!(Ok(vec!(-1,-1,-1,-1,-1,-1,-2,-2)),       (-10).split_into(8));
+    assert_eq!(Ok(vec!(-1,-1,-1,-1,-1,-1,-1,-1,-2)),    (-10).split_into(9));
+    assert_eq!(Ok(vec!(-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)), (-10).split_into(10));
 }
 
 #[test]
-fn test_split_into_zero_divident() {
-    assert_eq!(Ok(vec!(0)),                   split_into(0,1));
-    assert_eq!(Ok(vec!(0,0)),                 split_into(0,2));
-    assert_eq!(Ok(vec!(0,0,0)),               split_into(0,3));
-    assert_eq!(Ok(vec!(0,0,0,0)),             split_into(0,4));
-    assert_eq!(Ok(vec!(0,0,0,0,0)),           split_into(0,5));
-    assert_eq!(Ok(vec!(0,0,0,0,0,0)),         split_into(0,6));
-    assert_eq!(Ok(vec!(0,0,0,0,0,0,0)),       split_into(0,7));
-    assert_eq!(Ok(vec!(0,0,0,0,0,0,0,0)),     split_into(0,8));
-    assert_eq!(Ok(vec!(0,0,0,0,0,0,0,0,0)),   split_into(0,9));
-    assert_eq!(Ok(vec!(0,0,0,0,0,0,0,0,0,0)), split_into(0,10));
+fn test_split_into_zero_dividend() {
+    assert_eq!(Ok(vec!(0)),                   0.split_into(1));
+    assert_eq!(Ok(vec!(0,0)),                 0.split_into(2));
+    assert_eq!(Ok(vec!(0,0,0)),               0.split_into(3));
+    assert_eq!(Ok(vec!(0,0,0,0)),             0.split_into(4));
+    assert_eq!(Ok(vec!(0,0,0,0,0)),           0.split_into(5));
+    assert_eq!(Ok(vec!(0,0,0,0,0,0)),         0.split_into(6));
+    assert_eq!(Ok(vec!(0,0,0,0,0,0,0)),       0.split_into(7));
+    assert_eq!(Ok(vec!(0,0,0,0,0,0,0,0)),     0.split_into(8));
+    assert_eq!(Ok(vec!(0,0,0,0,0,0,0,0,0)),   0.split_into(9));
+    assert_eq!(Ok(vec!(0,0,0,0,0,0,0,0,0,0)), 0.split_into(10));
 }
