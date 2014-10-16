ffdeb4dc2fa72fcbb9fbb0fde23579f07a58aa61
Farrel Lifson <farrel.lifson@aimred.com>
2014-10-16 14:47:04 +0200

    Proper ; placement

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,9 @@ pub trait SplitInto {
 impl SplitInto for int {
     fn split_into(&self, divisor: uint) -> Result<Vec<int>, &'static str> {
         let dividend = *self;
-        if divisor == 0                     { return Err(DIVISOR_IS_ZERO) };
-        if dividend == 0                    { return Ok(Vec::from_elem(divisor,0i)) };
-        if divisor > dividend.abs() as uint { return Err(DIVISOR_LARGER_THAN_DIVIDEND) }
+        if divisor == 0                     { return Err(DIVISOR_IS_ZERO); }
+        if dividend == 0                    { return Ok(Vec::from_elem(divisor,0i)); }
+        if divisor > dividend.abs() as uint { return Err(DIVISOR_LARGER_THAN_DIVIDEND); }
 
         let quotient  = dividend / divisor as int;
         let remainder = dividend.abs() as uint % divisor;
